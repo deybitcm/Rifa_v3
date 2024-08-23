2e9494776dd39109e3cc90b400599e3223347889
Deybit Condori <david000anthony@gmail.com>
2024-08-22 22:09:56 -0500

    refactor: Update styles to use CSS class for desactivated premios

--- script.js
+++ script.js
@@ -142,9 +142,7 @@ function actualizarListas() {
     premios.forEach((premio, index) => {
         const li = document.createElement('li');
         if(premio.estado === false){
-            li.style.backgroundColor = 'red'; // Resalta el premio seleccionado
-            //cambiar color de texto
-            li.style.color = 'white';
+            li.classList.add('desactivado');
         }
         else {
             li.onclick = function () {
